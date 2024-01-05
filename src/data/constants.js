b7d1679ac09d1a7e7cfc4ef9ff10d6857f444084
Vishnu CP <vishnucpmcd@gmail.com>
2024-01-05 09:17:34 +0530

    skills icon updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -80,7 +80,7 @@ export const skills = [
       {
         name: "MongoDB",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+          "https://static-00.iconduck.com/assets.00/mongodb-compass-icon-512x511-15uiytr9.png",
       },
       // {
       //   name: "Graph Ql",
@@ -137,7 +137,7 @@ export const skills = [
       {
         name: "Git",
         image:
-          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+          "https://cdn-icons-png.flaticon.com/512/4494/4494740.png",
       },
       {
         name: "GitHub",
@@ -162,7 +162,7 @@ export const skills = [
       {
         name: "Postman",
         image:
-          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
       },
       // {
       //   name: "Adobe XD",
